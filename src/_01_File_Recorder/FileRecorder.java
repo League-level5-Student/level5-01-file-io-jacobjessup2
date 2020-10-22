8cd6fa07febe46d66197ed8b66021ed17ffd6970
monij <monij@DESKTOP-4MC4PHB.home>
2020-10-21 17:28:31 -0700

    File Recorder(finished)

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,23 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String message = JOptionPane.showInputDialog("Save a message to a file");
+		
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/Message1.txt", true);
+			
+			fw.write("\n"+message);
+				
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
